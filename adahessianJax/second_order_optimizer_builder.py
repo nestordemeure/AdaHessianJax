da94776029205741deaaedbcfaddf4b12e32cb17
Nestor Demeure <nestordemeure@gmail.com>
2020-10-10 19:01:27 +0200

    cleaner optimizer builder code

--- adahessianJax/second_order_optimizer_builder.py
+++ adahessianJax/second_order_optimizer_builder.py
@@ -5,34 +5,32 @@
 
 from typing import Callable, NamedTuple, Tuple, Any
 from collections import namedtuple
-import functools
-import numpy.random as npr
-from jax import grad, random
+from functools import wraps
+from jax import random
 from jax.util import partial, unzip2, safe_zip, safe_map
 from jax.tree_util import tree_flatten, tree_unflatten, register_pytree_node
-from jax.experimental.optimizers import Step, Updates, OptimizerState, InitFn, ParamsFn, Params, State, Optimizer
+from jax.experimental.optimizers import Step, Updates, OptimizerState, ParamsFn, Params, State, Optimizer
 
 from adahessianJax.hessian_computation import hutchinson_grad_and_hessian
 
-InitFn = Callable[[Params], OptimizerState]
-
-Rng = Any
-SecondOrderInitFn = Callable[[Params, Rng], OptimizerState]
-SecondOrderUpdateFn = Callable[[Step, Updates, Updates, OptimizerState], OptimizerState]
-
+# stores the optimizer's information
 SecondOrderOptimizerState = namedtuple("OptimizerState", ["packed_state", "tree_def", "subtree_defs", "rng"])
 register_pytree_node( # see https://jax.readthedocs.io/en/latest/pytrees.html
     SecondOrderOptimizerState,
     lambda xs: ((xs.packed_state,xs.rng), (xs.tree_def, xs.subtree_defs)), # flatten
     lambda data, xs: SecondOrderOptimizerState(xs[0], data[0], data[1], xs[1])) # unflatten
 
+# describes the optimizer's functions
+RngKey = Any
+SecondOrderInitFn = Callable[[Params, RngKey], OptimizerState]
+SecondOrderUpdateFn = Callable[[Step, Updates, Updates, OptimizerState], OptimizerState]
 class SecondOrderOptimizer(NamedTuple):
     init_fn: SecondOrderInitFn
     update_fn: SecondOrderUpdateFn
     params_fn: ParamsFn
 
 def second_order_optimizer(opt_maker: Callable[...,
-    Tuple[Callable[[Params, Rng], State],
+    Tuple[Callable[[Params, RngKey], State],
           Callable[[Step, Updates, Updates, Params], Params],
           Callable[[State], Params]]]) -> Callable[..., Optimizer]:
     """Decorator to make an optimizer defined for arrays generalize to containers.
@@ -41,11 +39,11 @@ def second_order_optimizer(opt_maker: Callable[...,
     functions that operate on pytrees of parameters.
     See the optimizer defined in adahessian.py for an example.
     """
-    @functools.wraps(opt_maker)
+    @wraps(opt_maker)
     def tree_opt_maker(*args, **kwargs):
         init, update, get_params = opt_maker(*args, **kwargs)
 
-        @functools.wraps(init)
+        @wraps(init)
         def tree_init(x0_tree, rng):
             "takes the network paramaters plus a Jax random generator key"
             x0_flat, tree = tree_flatten(x0_tree)
@@ -53,7 +51,7 @@ def second_order_optimizer(opt_maker: Callable[...,
             states_flat, subtrees = unzip2(safe_map(tree_flatten, initial_states))
             return SecondOrderOptimizerState(states_flat, tree, subtrees, rng)
 
-        @functools.wraps(update)
+        @wraps(update)
         def tree_update(i, loss, loss_input, opt_state, argnums=0):
             "takes the step number, the loss function, the input for the loss function, the optimizer state and the argnum information for the gradient"
             states_flat, tree_opt_state, subtrees, rng = opt_state
@@ -74,7 +72,7 @@ def second_order_optimizer(opt_maker: Callable[...,
                     raise TypeError(msg.format(subtree, subtree2))
             return SecondOrderOptimizerState(new_states_flat, tree_opt_state, subtrees, rng)
 
-        @functools.wraps(get_params)
+        @wraps(get_params)
         def tree_get_params(opt_state):
             states_flat, tree, subtrees, rng = opt_state
             states = safe_map(tree_unflatten, subtrees, states_flat)
