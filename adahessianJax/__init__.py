32b2dcef6fd76b48fdf03bb1a979a97c674b87ec
Nestor Demeure <nestordemeure@gmail.com>
2021-03-11 14:09:19 +0100

    tried new import

--- adahessianJax/__init__.py
+++ adahessianJax/__init__.py
@@ -1,1 +1,5 @@
 from .hessian_computation import grad_and_hessian, value_grad_and_hessian
+from . import jaxOptimizer
+from . import flaxOptimizer
+
+__all__ = ['jaxOptimizer', 'flaxOptimizer', 'grad_and_hessian', 'value_grad_and_hessian']
