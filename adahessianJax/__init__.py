a636d8d38b880ee5394923d79e2b5199ab7e2a1d
Nestor Demeure <nestordemeure@gmail.com>
2021-03-11 13:13:38 +0100

    changed import formulation in init

--- adahessianJax/__init__.py
+++ adahessianJax/__init__.py
@@ -1,5 +1,3 @@
-from adahessianJax.hessian_computation import grad_and_hessian, value_grad_and_hessian
-from adahessianJax import jax
-from adahessianJax import flax
+from .hessian_computation import grad_and_hessian, value_grad_and_hessian
 
-__all__ = ['jax', 'flax', 'grad_and_hessian', 'value_grad_and_hessian']
+__all__ = ['grad_and_hessian', 'value_grad_and_hessian']
--- adahessianJax/flax/__init__.py
+++ adahessianJax/flax/__init__.py
@@ -1,4 +1,4 @@
-from adahessianJax.flax.second_order_optimizer_builder import SecondOrderOptimizer, SecondOrderOptimizerDef
-from adahessianJax.flax.adahessian import Adahessian
+from .second_order_optimizer_builder import SecondOrderOptimizer, SecondOrderOptimizerDef
+from .adahessian import Adahessian
 
 __all__ = ['Adahessian', 'SecondOrderOptimizer', 'SecondOrderOptimizerDef']
