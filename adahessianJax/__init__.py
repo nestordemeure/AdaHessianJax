c8f20bd7c855e0dc7c5f350c60e8ae15b3558551
Nestor Demeure <nestordemeure@gmail.com>
2021-03-11 18:41:53 +0100

    updated imports

--- adahessianJax/__init__.py
+++ adahessianJax/__init__.py
@@ -1,3 +1,5 @@
-from .hessian_computation import grad_and_hessian, value_grad_and_hessian
-from . import jax
-from . import flax
+from adahessianJax.hessian_computation import grad_and_hessian, value_grad_and_hessian
+from adahessianJax import jax
+from adahessianJax import flax
+
+__all__ = ['jax', 'flax', 'grad_and_hessian', 'value_grad_and_hessian']
--- adahessianJax/jax/__init__.py
+++ adahessianJax/jax/__init__.py
@@ -1,4 +1,4 @@
-from .second_order_optimizer_builder import second_order_optimizer, SecondOrderOptimizer
-from .adahessian import adahessian
+from adahessianJax.jax.second_order_optimizer_builder import second_order_optimizer, SecondOrderOptimizer
+from adahessianJax.jax.adahessian import adahessian
 
-#__all__ = ['adahessian', 'second_order_optimizer', 'SecondOrderOptimizer']
+__all__ = ['adahessian', 'second_order_optimizer', 'SecondOrderOptimizer']
